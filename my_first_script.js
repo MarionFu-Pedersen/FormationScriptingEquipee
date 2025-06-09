862aebe6c7c8d81ee0375cc452905db6356e4f57
MarionFu-Pedersen <pro.marionfu.ped@gmail.com>
2025-06-09 14:36:45 +0200

    Correction-FautedeFrappe.js
    Correction de la faute de frappe

--- my_first_script.js
+++ my_first_script.js
@@ -2,5 +2,5 @@ function my_func() {
     MessageBox.information("hello world")
     MessageBox.information("byebye world")
 }
-//Changement fait sur GitHub pour simuler un changement d'une autre personne. Démontration de la fonction PULL
+//Changement fait sur GitHub pour simuler un changement d'une autre personne. Démonstration de la fonction PULL
 

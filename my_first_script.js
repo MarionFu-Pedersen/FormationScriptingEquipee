5e3c0ab2d97ad785076f0c61618aa1740a0a8a54
MarionFu-Pedersen <pro.marionfu.ped@gmail.com>
2025-06-10 09:27:24 +0200

    Update my_first_script.js
    Fin de journée du 09/06

--- my_first_script.js
+++ my_first_script.js
@@ -1,6 +1,21 @@
 function my_func() {
-    MessageBox.information("hello world")
-    MessageBox.information("byebye world")
-}
-//Changement fait sur GitHub pour simuler un changement d'une autre personne. Démonstration de la fonction PULL
+// déclaration de la variable my_var et assignation d'une valeur de 1
+var my_var = 2
 
+my_var = 3
+
+var my_var2 = 10
+
+my_var = my_var2
+
+my_var3 = my_var + my_var2
+my_var4 = my_var3 - my_var2
+MessageBox.information("variable 4 ="+ my_var4)
+var my_var5 = my_var4 / my_var3
+MessageBox.information("variable 5 =" + my_var5)
+var my_var6 = my_var4 * ( 3 + 10 )
+
+my_var6 = my_var6 + 1
+
+
+}

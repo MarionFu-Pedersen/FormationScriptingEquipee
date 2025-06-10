b741c39466f5a1b8f2e589b4c73b3b6b2fc050f5
MarionFu-Pedersen <pro.marionfu.ped@gmail.com>
2025-06-10 09:53:51 +0200

    Booleans
    Cours sur les booléens

--- my_first_script.js
+++ my_first_script.js
@@ -1,21 +1,13 @@
 function my_func() {
-// déclaration de la variable my_var et assignation d'une valeur de 1
-var my_var = 2
 
-my_var = 3
+var bool = true
+bool = false
+MessageBox.information(mybool)
+}function 
 
-var my_var2 = 10
+my_func2() {
 
-my_var = my_var2
-
-my_var3 = my_var + my_var2
-my_var4 = my_var3 - my_var2
-MessageBox.information("variable 4 ="+ my_var4)
-var my_var5 = my_var4 / my_var3
-MessageBox.information("variable 5 =" + my_var5)
-var my_var6 = my_var4 * ( 3 + 10 )
-
-my_var6 = my_var6 + 1
-
-
+var bool = true
+bool = false
+MessageBox.information(mybool)
 }

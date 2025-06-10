73949c26cec07a5e5c0566de2d6cde6325e938a0
MarionFu-Pedersen <pro.marionfu.ped@gmail.com>
2025-06-10 12:31:54 +0200

    Plot_Twist
    Les strings sont des array

--- my_first_script.js
+++ my_first_script.js
@@ -31,7 +31,7 @@ message = message + heart //auto-référence
 message += heart // simplification de l'opérateur. Ne pas remettre var
 message += heart
 message += heart
-MessageBox.information(message)
+MessageLog.trace(message)
 
 //Partie 3 : Array
 var array = ["Oui ","Non ","Peut-être "]
@@ -39,5 +39,26 @@ MessageLog.trace(array[1]) // Attention!! l'index commence à 0 comme un ascence
 var my_index = 2
 my_string =  array[0] + array[my_index] 
 MessageLog.trace(my_string)
+
+//Ajouter des valeurs dans l'array
+var array2 = []
+array2.push("Head") // array2[0] = Head
+array2.push("Torso") // array2[1] = Torso
+array2.push("Arm L") // etc
+array2.push("Arm R")
+array2.push("Leg L")
+array2.push("Leg R")
+MessageLog.trace(array2)
+
+//plot twist : les strings sont des array, les synthaxes d'array s'appliquent donc de la même manière aux strings
+var mots1 = "Camion"
+  var mots2 = "Olive"
+  var mots3 = "Original"
+  var mots4 = "Long"
+
+  var mots_cache = mots1[0] + mots2[0] + mots3[0] + mots4[0]
+  MessageBox.information(mots_cache)
+
+// array à deux dimensions : array dans une array. Array à x dimensions, array dans une array x fois.
 
 }

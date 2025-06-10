dad9ca80d3785e65463f5548840097b27dc43c67
MarionFu-Pedersen <pro.marionfu.ped@gmail.com>
2025-06-10 12:05:29 +0200

    Introduction des arrays

--- my_first_script.js
+++ my_first_script.js
@@ -1,4 +1,4 @@
-// Fonction  "Awkward monkey" dans ToonBoom
+// Fonction1  "Awkward monkey" dans ToonBoom, test
 function my_func() {
 
 var bool = true
@@ -6,13 +6,38 @@ bool = true
 MessageBox.information(bool)
 }function 
 
-// Fonction "This is fine" dans ToonBoom
+/* Fonction2 :  "This is fine" dans ToonBoom. Sert à tester les nouvelles notions de cours
+à savoir  
+- les booleans (my_var)
+- les strings "Hello World" avec des guillemets \"
+- les arrays
+
+*/
+
 my_func2() {
 
+//Partie 1 : Booléens
 var A = 10
 var B = 30
-var my_var = A < B
+var my_var = A < B 
+//Partie 2 : String
+var heart = "❤️" 
 if (my_var) 
-    { MessageBox.information("\"Hello World\"") }
+    { MessageBox.information("\"Hello World\"  ٩(◕‿◕｡)۶ "+ heart + heart ) }
+
+var phrase = "hi"
+var message = phrase + heart
+message = message + heart //auto-référence
+message += heart // simplification de l'opérateur. Ne pas remettre var
+message += heart
+message += heart
+MessageBox.information(message)
+
+//Partie 3 : Array
+var array = ["Oui ","Non ","Peut-être "]
+MessageLog.trace(array[1]) // Attention!! l'index commence à 0 comme un ascenceur qui commence à 0 
+var my_index = 2
+my_string =  array[0] + array[my_index] 
+MessageLog.trace(my_string)
 
 }

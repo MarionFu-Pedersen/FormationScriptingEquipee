f3df95dd102f51793ae0c933990a82942522487c
MarionFu-Pedersen <pro.marionfu.ped@gmail.com>
2025-06-11 15:46:15 +0200

    Start of exo howmanypegs

--- exo_howmanypegs.js
+++ exo_howmanypegs.js
@@ -1,11 +1,11 @@
 // cet exercice simule un sanity check de rig
 
-function how_many_pegs(){
+function show_how_many_pegs(){
     var peg_list = node.getNodes(["PEG"])
-    var nb_peg = peg_list.length 
-    return nb_peg
+    MessageLog.trace(peg_list)
+    var total_nb_peg = peg_list.length
+    MessageLog.trace(total_nb_peg)
+
 }
-function nothing(){
-    
-}
+
 
